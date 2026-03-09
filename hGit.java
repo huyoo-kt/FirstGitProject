c967603ee191c1561e63b39fcf49ea338055bdd1
huyoo <juliannekurtt@gmail.com>
2026-03-09 18:31:29 +0800

    hi ako ito

--- hGit.java
+++ hGit.java
@@ -1,5 +1,6 @@
-class hGit {
+class hGit {    
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
+        System.out.println("Heres my second try");
     }
 }
